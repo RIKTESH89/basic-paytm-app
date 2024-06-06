791844c377a747ba46b3fcae256d64ac3f098109
RIKTESH89 <rikteshsingh28801@gmail.com>
2024-06-06 19:57:30 +0530

    vercel.json

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,10 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "",
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ["react-router-dom"],
-    },
-  },
 });
